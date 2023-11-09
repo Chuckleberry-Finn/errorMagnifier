7c0cd1e76b6d55fdea6eed7c50f8fc479211194b
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-09 12:56:52 -0500

    donate

---
+++ Contents/mods/errorMagnifier/media/lua/client/errorMagnifier_donate.lua
@@ -0,0 +1,4 @@
+local donationSystem = require "chuckleberryFinnDonate"
+donationSystem.addModCount()
+--donationSystem.addTexture("media/textures/donate/1.png")
+--donationSystem.addTexture("media/textures/donate/2.png")
