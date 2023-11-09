56954136b8478f0aa72d93459b9c4751ff945b78
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-09 14:59:00 -0500

    Delete errorMagnifier_donate.lua

--- Contents/mods/errorMagnifier/media/lua/client/errorMagnifier_donate.lua
+++
@@ -1,5 +0,0 @@
-local donationSystem = require "chuckleberryFinnDonate"
-if not donationSystem then return end
-donationSystem.addModCount()
---donationSystem.addTexture("media/textures/donate/1.png")
---donationSystem.addTexture("media/textures/donate/2.png")
