147093cfd0e8a3c211df4cc4a13fe35f3d9f2b5f
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-12-21 17:31:45 -0500

    B42

--- Contents/mods/errorMagnifier/42.0/media/lua/client/fingerPrint_Main.lua
+++ Contents/mods/errorMagnifier/42.0/media/lua/client/fingerPrint_Main.lua
@@ -15,21 +15,21 @@ function _G.print(...)
 	local printText
 	if coroutine then
 		local count = getCallframeTop(coroutine)
-		for i= count - 1, 0, -1 do
+		--for i= count - 1, 0, -1 do
 			---@type LuaCallFrame
-			local luaCallFrame = getCoroutineCallframeStack(coroutine,i)
+			local luaCallFrame = getCoroutineCallframeStack(coroutine,0)
 			if luaCallFrame ~= nil and luaCallFrame then
 				local fileDir = getFilenameOfCallframe(luaCallFrame)
-				if fileDir then
-					local modInfoDir = fileDir:match("(.-)media/")
-					local modInfo = modInfoDir and getModInfoByID(modInfoDir)
+				if fileDir and fileDir ~= "" then
+					local modInfoDir = fileDir:match("^(.*/Contents/mods/[^/]+/)")
+					local modInfo = modInfoDir and getModInfo(modInfoDir)
 					local modID = modInfo and modInfo:getId()
-					if modID and modID~="errorMagnifier" then
+					if modID then
 						printText = "\["..modID.."\] "
 					end
 				end
 			end
-		end
+		--end
 	end
 
 	local args = {...}
@@ -45,9 +45,3 @@ function _G.print(...)
 		printThis((printText and printText or "") .. message)
 	end
 end
-
-
-if getActivatedMods():contains("ChuckleberryFinnAlertSystem") then
-	local modCountSystem = require "chuckleberryFinnModding_modCountSystem"
-	if modCountSystem then modCountSystem.pullAndAddModID() end
-else print("WARNING: Highly recommended to install `ChuckleberryFinnAlertSystem` (Workshop ID: `3077900375`) for latest news and updates.") end
