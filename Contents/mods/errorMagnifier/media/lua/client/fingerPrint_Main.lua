03254f4785694f8e65e70baf54d7ee72d385fbb6
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-12-23 01:16:16 -0500

    Update fingerPrint_Main.lua

--- Contents/mods/errorMagnifier/media/lua/client/fingerPrint_Main.lua
+++ Contents/mods/errorMagnifier/media/lua/client/fingerPrint_Main.lua
@@ -28,8 +28,3 @@ function _G.print(...)
 		print_original(...)
 	end
 end
-
-if getActivatedMods():contains("ChuckleberryFinnAlertSystem") then
-	local modCountSystem = require "chuckleberryFinnModding_modCountSystem"
-	if modCountSystem then modCountSystem.pullAndAddModID() end
-else print("WARNING: Highly recommended to install `ChuckleberryFinnAlertSystem` (Workshop ID: `3077900375`) for latest news and updates.") end
