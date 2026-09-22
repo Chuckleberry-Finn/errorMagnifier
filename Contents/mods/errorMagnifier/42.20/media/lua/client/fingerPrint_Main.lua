e62512fb10a03e9ff31ecd82caceb2dfdb9d60b9
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2026-09-22 00:40:46 -0400

    debuglog

--- Contents/mods/errorMagnifier/42.20/media/lua/client/fingerPrint_Main.lua
+++ Contents/mods/errorMagnifier/42.20/media/lua/client/fingerPrint_Main.lua
@@ -1,52 +1,30 @@
-local print_original = print
---[[
-local writer = getFileWriter("TEMP_PRINT_LOG.txt", true, false)
-writer:close()
-
-local function printThis(msg)
-	local w = getFileWriter("TEMP_PRINT_LOG.txt", true, true)
-	w:write(msg.."\n")
-	w:close()
-end
---]]
 function _G.print(...)
 
 	local coroutine = getCurrentCoroutine()
 	local modTag
 	if coroutine then
-		-- local count = getCallframeTop(coroutine)
-		--for i= count - 1, 0, -1 do
-			---@type LuaCallFrame
-			local luaCallFrame = getCoroutineCallframeStack(coroutine,0)
-			if luaCallFrame ~= nil and luaCallFrame then
-				local fileDir = getFilenameOfCallframe(luaCallFrame)
-				if fileDir and fileDir ~= "" then
-					local modInfoDir = fileDir:match("^(.*/Contents/mods/[^/]+/)")
-					local modInfo = modInfoDir and getModInfo(modInfoDir)
-					local modID = modInfo and modInfo:getId()
-					if modID then
-						modTag = "["..modID.."] "
-					end
+
+		local luaCallFrame = getCoroutineCallframeStack(coroutine,0)
+		if luaCallFrame ~= nil and luaCallFrame then
+			local fileDir = getFilenameOfCallframe(luaCallFrame)
+			if fileDir and fileDir ~= "" then
+				local modInfoDir = fileDir:match("^(.*/Contents/mods/[^/]+/)")
+				local modInfo = modInfoDir and getModInfo(modInfoDir)
+				local modID = modInfo and modInfo:getId()
+				if modID then
+					modTag = "["..modID.."] "
 				end
 			end
-		--end
-	end
-
-	if modTag and modTag ~= "" then
-		print_original(":", modTag, ...)
-	else
-		print_original(":", ...)
-	end
-	--[[
-	if getDebug() then
-		local args = {...}
-		local message = nil--table.concat(args, " ")
-
-		for i = 1, #args do
-			message = (message or "") .. tostring(args[i]) .. " "
 		end
 
-		printThis(":" .. ((modTag and modTag or "[ vanilla ] ") .. tostring(message)))
 	end
-	--]]
+
+	local output = modTag and modTag ~= "" and modTag or ""
+
+	for i = 1, select('#', ...) do
+		local val = select(i, ...)
+		output = output .. "    " .. tostring(val)
+	end
+
+	DebugLog.log(output)
 end
